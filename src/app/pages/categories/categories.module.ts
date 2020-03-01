797983a0c2993947f2e29559f9e3800cf4903c2f
Lucas de Freitas <lucasffm@gmail.com>
2020-03-01 20:03:24 -0300

    List categories

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,16 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { CategoriesRoutingModule } from './categories-routing.module';
-import { CategoryListComponent } from './category-list/category-list.component';
-import { CategoryFormComponent } from './category-form/category-form.component';
-
+import { CategoriesRoutingModule } from "./categories-routing.module";
+import { CategoryListComponent } from "./category-list/category-list.component";
+import { CategoryFormComponent } from "./category-form/category-form.component";
+import { CategoryService } from "./shared/category.service";
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryFormComponent],
-  imports: [
-    CommonModule,
-    CategoriesRoutingModule
-  ]
+  providers: [CategoryService],
+  imports: [CommonModule, CategoriesRoutingModule]
 })
-export class CategoriesModule { }
+export class CategoriesModule {}
--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Category } from "../shared/category.model";
+import { CategoryService } from "../shared/category.service";
 
 @Component({
-  selector: 'app-category-list',
-  templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.css']
+  selector: "app-category-list",
+  templateUrl: "./category-list.component.html",
+  styleUrls: ["./category-list.component.css"]
 })
 export class CategoryListComponent implements OnInit {
-
-  constructor() { }
+  categories: Category[] = [];
+  constructor(private service: CategoryService) {}
 
   ngOnInit(): void {
+    this.service.getAll().subscribe(
+      categories => (this.categories = categories),
+      error => console.log("Erro ao carregar a lista", error)
+    );
   }
 
+  deleteCategory(category: Category) {
+    const mustDelete = confirm("Deseja Realmente excluir este item?");
+    if (mustDelete) {
+      this.service.delete(category.id).subscribe(
+        () =>
+          (this.categories = this.categories.filter(cat => cat !== category)),
+        error => console.log("Erro ao carregar a lista", error)
+      );
+    }
+  }
 }
