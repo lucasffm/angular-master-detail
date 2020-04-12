f9870f0b515b79864a4d3a8171823f807a00a84c
Lucas de Freitas <lucasffm@gmail.com>
2020-04-11 21:41:52 -0300

    :sparkles: Setup entries module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: "categories",
+    path: 'categories',
     loadChildren: () =>
-      import("./pages/categories/categories.module").then(
-        m => m.CategoriesModule
-      )
-  }
+      import('./pages/categories/categories.module').then(
+        (m) => m.CategoriesModule
+      ),
+  },
+  {
+    path: 'entries',
+    loadChildren: () =>
+      import('./pages/entries/entries.module').then((m) => m.EntriesModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
